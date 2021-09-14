6d56517cc693e9d17f96bb8ccc83628cba9291b9
raza <raza.ahmad.khan786@gmail.com>
2021-09-14 16:39:36 +0530

    swagger working perfectly

--- src/main/java/com/mycompany/springbootproject/controller/CustomerController.java
+++ src/main/java/com/mycompany/springbootproject/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.mycompany.springbootproject.controller;
 
 import com.mycompany.springbootproject.model.Customer;
 import com.mycompany.springbootproject.service.CustomerService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,11 +22,13 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping("/getall")
+    @ApiOperation("Fetch All Customer Records")
     public List<Customer> getCustomers() {
         log.info("INSIDE getCustomers!!");
         return customerService.getAll();
     }
 
+    @ApiOperation("Save a New Customer Record!")
     @PostMapping("/create")
     public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer) {
         log.info("Inside Customer %s", customer);
@@ -33,18 +36,20 @@ public class CustomerController {
         return new ResponseEntity<>(cust, HttpStatus.CREATED);
     }
 
+    @ApiOperation("Get A Customer By ID")
     @GetMapping("/getbyid/{id}")
     public Customer fetchById(@PathVariable int id) {
         return customerService.fetchById(id);
     }
 
-
+    @ApiOperation("Update An Existing Customer")
     @PutMapping("/update/{id}")
     public void update(@PathVariable int id,@Valid @RequestBody Customer customer){
         log.info("Updating a customer!!");
         customerService.udpateCustomer(id, customer);
     }
 
+    @ApiOperation("Delete A Customer")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id){
         log.info("Deleting a customer with id %d", id);
